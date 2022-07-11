a4fe5467e515baad53159a5013816757eac74206
Elliot-IA <alexa818@umn.edu>
2022-07-11 12:22:42 -0500

    autoCommit

--- js/_.js
+++ js/_.js
@@ -2,5 +2,6 @@ window.onload = ()=>{
     a("activatePolyBtn").onclick =()=>{
         a("polyFrame").src = "https://poly.cam/capture/64365421-F546-46C2-90AB-B86434DDAE5A";
         a("activatePolyBtn").style = "display: none;"
+        a("3DGoldy").style = "display: none;"
     }
 }
